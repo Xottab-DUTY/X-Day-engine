f1a0cf2b688fa1c06e7b0f23f9050f2840a47f59
Xottab-DUTY <xottab-duty@yandex.ru>
2017-04-21 16:28:22 +0500

    Work on Renderer

--- src/xdRender/xdRender.cpp
+++ src/xdRender/xdRender.cpp
@@ -1,2 +1,44 @@
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+
+#include "xdEngine/xdCore.hpp"
+#include "xdEngine/Console.hpp"
 #include "xdRender.hpp"
 
+RENDERER_API Renderer Render;
+
+Renderer::Renderer()
+{
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pNext = NULL;
+    applicationInfo.pApplicationName = Core.AppName.c_str();
+    applicationInfo.pEngineName = Core.EngineName.c_str();
+    applicationInfo.engineVersion = std::stoi(Core.EngineVersion); // Engine development version
+    applicationInfo.apiVersion = VK_API_VERSION_1_0;
+
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pNext = NULL;
+    instanceInfo.flags = 0; // flags, 0 for now
+    instanceInfo.pApplicationInfo = &applicationInfo;
+    instanceInfo.enabledLayerCount = 0; // disable layers
+    instanceInfo.ppEnabledLayerNames = NULL;
+    instanceInfo.enabledExtensionCount = 0; // disable extensions
+    instanceInfo.ppEnabledExtensionNames = NULL;
+}
+
+void Renderer::Initialize()
+{
+    PFN_vkCreateInstance pfnCreateInstance = (PFN_vkCreateInstance)
+        glfwGetInstanceProcAddress(nullptr, "vkCreateInstance");
+    
+
+    
+
+    //PFN_vkCreateDevice pfnCreateDevice = (PFN_vkCreateDevice)
+    //    glfwGetInstanceProcAddress(instance, "vkCreateDevice");
+}
+
+void Renderer::Destroy()
+{
+
+}
--- src/xdRender/xdRender.hpp
+++ src/xdRender/xdRender.hpp
@@ -1,7 +1,24 @@
 #pragma  once
 #ifndef xdRender_hpp__
 #define xdRender_hpp__
-
 #include "xdRender_impexp.inl"
 
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+
+class RENDERER_API Renderer
+{
+public:
+    VkApplicationInfo applicationInfo;
+    VkInstanceCreateInfo instanceInfo;
+    VkInstance instance;
+    VkResult result;
+
+    Renderer();
+    void Initialize();
+    void Destroy();
+};
+
 #endif // xdRender_hpp__
+
+extern RENDERER_API Renderer Render;
