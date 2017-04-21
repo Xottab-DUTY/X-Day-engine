57a4c42b1e880a18e4fecd1a9ac1ce51e082051f
Xottab-DUTY <xottab-duty@yandex.ru>
2017-04-21 16:25:34 +0500

    Call ExitCC instead of direct bool changing

--- src/xdEngine/xdEngine.cpp
+++ src/xdEngine/xdEngine.cpp
@@ -13,11 +13,9 @@ XDAY_API XDayEngine Engine;
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        ConsoleCommands->Execute(&ExitCC);
     if (key == GLFW_KEY_ENTER && action == GLFW_PRESS)
-    {
         ConsoleCommands->Execute(fmt::format("r_fullscreen {}", !ConsoleCommands->GetBool("r_fullscreen")));
-    }
 }
 
 void XDayEngine::Initialize()
