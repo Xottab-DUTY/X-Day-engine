fc0bd0b9742280a69556343f172973d2a7dff2b4
Xottab-DUTY <xottab-duty@yandex.ru>
2017-05-06 22:42:30 +0500

    Cut off debug messages

--- src/xdEngine/XMLDocument.cpp
+++ src/xdEngine/XMLDocument.cpp
@@ -37,35 +37,21 @@ xdXMLDocument::~xdXMLDocument()
 void xdXMLDocument::Load(filesystem::path resources_type, filesystem::path _path, std::string xml_filename)
 {
     std::string buffer(resources_type.string() + _path.string() + xml_filename);
-
-    if (xml_document.LoadFile(buffer.c_str()) == 0)
-        ConsoleMsg("XML file loaded: {}", buffer)
-    else
-        ConsoleMsg("XML failed to load file: {}", buffer);
+    xml_document.LoadFile(buffer.c_str());
     root_node = xml_document.FirstChild();
-    xml_document.Print();
 }
 
 void xdXMLDocument::Load(filesystem::path resources_type, std::string xml_filename)
 {
     std::string buffer(resources_type.string() + xml_filename);
-
-    if (xml_document.LoadFile(buffer.c_str()) == 0)
-        ConsoleMsg("XML file loaded: {}", buffer)
-    else
-        ConsoleMsg("XML failed to load file: {}", buffer);
+    xml_document.LoadFile(buffer.c_str());
     root_node = xml_document.FirstChild();
-    xml_document.Print();
 }
 
 void xdXMLDocument::Load(filesystem::path full_path)
 {
-    if (xml_document.LoadFile(full_path.string().c_str())) // string().c_str() what a nice costyl
-        ConsoleMsg("XML file loaded: {}", full_path.string())
-    else
-        ConsoleMsg("XML failed to load file: {}", full_path.string());
+    xml_document.LoadFile(full_path.string().c_str()); // string().c_str() what a nice costyl
     root_node = xml_document.FirstChild();
-    xml_document.Print();
 }
 
 bool xdXMLDocument::isErrored() const
