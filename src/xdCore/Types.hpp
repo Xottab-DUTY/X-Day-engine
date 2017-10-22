51ef1fd04b9412255f94d0a1572ed87c293a30f4
Xottab-DUTY <xottab-duty@yandex.ru>
2017-10-22 13:24:01 +0500

    Create new types that will use Intel TBB allocator Not used for now

---
+++ src/xdCore/Types.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <filesystem>
+namespace filesystem = std::experimental::filesystem;
+
+#include <tbb/cache_aligned_allocator.h>
+
+using pstr = char*;
+using pcstr = const char*;
+
+namespace XDay
+{
+template<class T>
+class allocator : public tbb::cache_aligned_allocator<T>;
+
+using string = std::basic_string<char, std::char_traits<char>, allocator<char>>;
+using wstring = std::basic_string<wchar_t, std::char_traits<wchar_t>, allocator<wchar_t>>;
+using u16string = std::basic_string<char16_t, std::char_traits<char16_t>, allocator<char16_t>>;
+using u32string = std::basic_string<char32_t, std::char_traits<char32_t>, allocator<char32_t>>;
+
+template<class T>
+class vector : public std::vector<T, allocator<T>>;
+}
