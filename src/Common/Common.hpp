f4419f725ae804d09d4d965cba4a87b81b4da2b4
Xottab-DUTY <xottab-duty@yandex.ru>
2018-05-24 00:13:21 +0500

    BasicTypes.hpp: removed wchar_t support, added string aliases IntegerTypes.hpp: added int types

--- src/Common/Common.hpp
+++ src/Common/Common.hpp
@@ -3,6 +3,7 @@
 #include "Common/Platform.hpp"
 #include "Common/CommonImportExport.inl"
 #include "Common/Types/BasicTypes.hpp"
+#include "Common/Types/IntegerTypes.hpp"
 #include "Common/Types/String.hpp"
 #include "Common/Types/Vector.hpp"
 
